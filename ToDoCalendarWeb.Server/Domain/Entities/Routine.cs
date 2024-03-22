bbc2cb33d32d32d70afcb28cfa422436b4e4dce5
nattflo <nattfloder@gmail.com>
2024-03-22 14:36:47 +0700

    Ingore useless fields

--- ToDoCalendarWeb.Server/Domain/Entities/Routine.cs
+++ ToDoCalendarWeb.Server/Domain/Entities/Routine.cs
@@ -1,11 +1,15 @@
-﻿namespace ToDoCalendarWeb.Domain;
+﻿using System.Text.Json.Serialization;
+
+namespace ToDoCalendarWeb.Domain;
 
 public class Routine : AbstractTrackableEntity 
 {
     public override Guid Id { get; set; }
     public required string Name { get; set; }
+    [JsonIgnore]
     public virtual List<Period>? Periods { get; set; }
     public string? Description { get; set; }
+    [JsonIgnore]
     public override bool IsTracked { get; set; } = true;
 
 }
